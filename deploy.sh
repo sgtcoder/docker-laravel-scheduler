b903f47ee7cd42ded43d63b04dec5f506e03cd00
sgtcoder <usmccoder@gmail.com>
2023-05-28 18:26:05 -0700

    Dockerfile update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="laravel-scheduler"
 
-docker build --no-cache  -t $REGISTRY/$PROJECT_NAME .
+docker build -t $REGISTRY/$PROJECT_NAME .
 docker push $REGISTRY/$PROJECT_NAME
